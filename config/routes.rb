668430db55905ab85eaa6cdd32895b979eee10ef
Ben S <45582717+nextbendev@users.noreply.github.com>
2020-05-15 02:08:09 -0400

    cleaned up partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'pages/login'
-  get 'pages/console'
-  get 'pages/profile'
-  get 'pages/message'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/pages/login', to: 'pages#login'
+  get '/pages/console', to: 'pages#console'
+  get '/pages/profile', to: 'pages#profile'
+  get '/pages/message', to: 'pages#message'
 end
